abf3a42523dfe1e4fdea31e13492627c6afab5f9
msivakumars <sivakumar.manickam@zemosolabs.com>
2024-07-21 17:29:35 +0530

    (jenkins-nodejs: name corrected)

--- job-dsl/nodejsdocker.groovy
+++ job-dsl/nodejsdocker.groovy
@@ -2,14 +2,14 @@ job('NodeJS Docker example') {
     scm {
         git('https://github.com/msivakumars2712/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
             node / gitConfigName('DSL User')
-            node / gitConfigEmail('jenkins-dsl@newtech.academy')
+            node / gitConfigEmail('sivakumar.manickam@zemosolabs.com')
         }
     }
     triggers {
         scm('H/5 * * * *')
     }
     wrappers {
-        nodejs('nodejs') // this is the name of the NodeJS installation in 
+        nodejs('node-js') // this is the name of the NodeJS installation in
                          // Manage Jenkins -> Configure Tools -> NodeJS Installations -> Name
     }
     steps {
